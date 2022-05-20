4292cd3176644def2b606c0e2b0719332c312afb
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-05-20 20:49:20 -0300

    requisito 1 ok 🦁

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getSpeciesByIds(...ids) {
-  // seu código aqui
+  if (ids === undefined) {
+    return [];
+  }
+  return species.filter((element) => ids.includes(element.id));
 }
 
 module.exports = getSpeciesByIds;
