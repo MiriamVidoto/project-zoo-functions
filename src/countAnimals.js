de0ddcd252f0e3f243c66bb8470630fb883d2eb7
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-05-22 17:11:30 -0300

    requisito 5 ok 🐼

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function countAnimals(...animal) {
+  if (animal.length === 0) {
+    return species.reduce((acc, crr) => {
+      acc[crr.name] = crr.residents.length;
+      return acc;
+    }, {});
+  }
+  const [myAnimal] = animal;
+  if (myAnimal.sex === undefined) {
+    return species.find((nameAnimal) => nameAnimal.name === myAnimal.specie).residents.length;
+  }
+  return species.find((nameAnimal) => nameAnimal.name === myAnimal.specie).residents
+    .filter((animalSex) => animalSex.sex === myAnimal.sex).length;
 }
+
+// console.log(countAnimals({ specie: 'penguins' }));
+
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
+
+// console.log(countAnimals());
 
 module.exports = countAnimals;
