27f31167cc6f1adb3f643edd7a591d8ad1811d10
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-05-22 14:16:39 -0300

    requisito 3 ok 🐷

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees
+    .find((element) => element.firstName === employeeName || element.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
