38e65f7aebe169c396278d53ebed8a6fc372bf3f
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-05-24 07:05:04 -0300

    requisito 8 ok 🐼

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -3,25 +3,39 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
+const result = (speciesResponsible, employeeRenponsible) => species
+  .filter((element) => speciesResponsible.includes(element.id))
+  .reduce((acc, crr) => {
+    acc.species += `${crr.name}`;
+    acc.locations += `${crr.location}`;
+    return acc;
+  }, { id: employeeRenponsible.id,
+    fullName: `${employeeRenponsible.firstName} ${employeeRenponsible.lastName}`,
+    species: '',
+    locations: '' });
+
 function getEmployeesCoverage(obj) {
+  if (obj === undefined) {
+    // const employee = employees.map((element) => element.id);
+    const employeeResponsibleFor = employees.map((ell) => ell.responsibleFor);
+    return employeeResponsibleFor;
+  }
   if (obj.id !== undefined) {
     const employee = employees.find((employeeId) => employeeId.id === obj.id);
-    const animalLocal = employee.responsibleFor
-      .forEach((animalId) => {
-        return species.find((element) => {
-          return element.id === animalId;
-        });
-      });
-      return animalLocal;
-    // return species.find((element) => element.id === animalId[0]).location;
+    const employeeResponsibleFor = employee.responsibleFor;
+    return result(employeeResponsibleFor, employee);
   }
-  // recebe obj.name ou ojb.id
-  // encontra a pessoa
-  // encontra as especies que a pessoa é responsável
-  // procura as localizações das espécies
-  // retorna um objeto
+  if (obj.name !== undefined) {
+    const employee = employees.find((employeeName) => employeeName.firstName === obj.name
+    || employeeName.lastName === obj.name);
+    const employeeResponsibleFor = employee.responsibleFor;
+    return result(employeeResponsibleFor, employee);
+  }
 }
 
-console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+// console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// console.log(getEmployeesCoverage({ name: 'Spry' }));
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
